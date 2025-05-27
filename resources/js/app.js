09fa50fe82a4af142e6c787d535b5e2cacc95e11
FafaRabbani <fafa.rabbanii@gmail.com>
2025-05-27 21:11:09 +0700

    update resource

--- resources/js/app.js
+++ resources/js/app.js
@@ -96,70 +96,70 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-const navDialog = document.getElementById("nav-dialog");
+// const navDialog = document.getElementById("nav-dialog");
 
-function handleMenu() {
-    navDialog.classList.toggle("hidden");
-}
+// function handleMenu() {
+//     navDialog.classList.toggle("hidden");
+// }
 
-// Scroll Apps Start
-const initialTranslateLTR = -48 * 4;
-const initialTranslateRTL = 36 * 4;
+// // Scroll Apps Start
+// const initialTranslateLTR = -48 * 4;
+// const initialTranslateRTL = 36 * 4;
 
-function setupIntersectionObeserver(element, isLTR, speed) {
-    const intersectionCallback = (entries) => {
-        const isIntersecting = entries[0].isIntersecting;
-        if (isIntersecting) {
-            document.addEventListener("scroll", scrollHandler);
-        } else {
-            document.removeEventListener("scroll", scrollHandler);
-        }
-    };
-    const intersectionObserver = new IntersectionObserver(intersectionCallback);
+// function setupIntersectionObeserver(element, isLTR, speed) {
+//     const intersectionCallback = (entries) => {
+//         const isIntersecting = entries[0].isIntersecting;
+//         if (isIntersecting) {
+//             document.addEventListener("scroll", scrollHandler);
+//         } else {
+//             document.removeEventListener("scroll", scrollHandler);
+//         }
+//     };
+//     const intersectionObserver = new IntersectionObserver(intersectionCallback);
 
-    intersectionObserver.observe(element);
+//     intersectionObserver.observe(element);
 
-    function scrollHandler() {
-        const translateX =
-            (window.innerHeight - element.getBoundingClientRect().top) * speed;
+//     function scrollHandler() {
+//         const translateX =
+//             (window.innerHeight - element.getBoundingClientRect().top) * speed;
 
-        let totalTranslate = 0;
-        if (isLTR) {
-            totalTranslate = translateX + initialTranslateLTR;
-        } else {
-            totalTranslate = -(translateX + initialTranslateRTL);
-        }
-        element.style.transform = `translateX(${totalTranslate}px)`;
-    }
-}
+//         let totalTranslate = 0;
+//         if (isLTR) {
+//             totalTranslate = translateX + initialTranslateLTR;
+//         } else {
+//             totalTranslate = -(translateX + initialTranslateRTL);
+//         }
+//         element.style.transform = `translateX(${totalTranslate}px)`;
+//     }
+// }
 
-const line1 = document.getElementById("line1");
-const line2 = document.getElementById("line2");
-const line3 = document.getElementById("line3");
+// const line1 = document.getElementById("line1");
+// const line2 = document.getElementById("line2");
+// const line3 = document.getElementById("line3");
 
-setupIntersectionObeserver(line1, true, 0.15);
-setupIntersectionObeserver(line2, false, 0.15);
-setupIntersectionObeserver(line3, true, 0.15);
+// setupIntersectionObeserver(line1, true, 0.15);
+// setupIntersectionObeserver(line2, false, 0.15);
+// setupIntersectionObeserver(line3, true, 0.15);
 
-document.addEventListener("DOMContentLoaded", function () {
-    // Ambil semua elemen dt
-    const toggles = document.querySelectorAll(".group dt");
+// document.addEventListener("DOMContentLoaded", function () {
+//     // Ambil semua elemen dt
+//     const toggles = document.querySelectorAll(".group dt");
 
-    toggles.forEach(function (toggle) {
-        toggle.addEventListener("click", function () {
-            const group = toggle.closest(".group");
-            const dd = group.querySelector("dd");
-            const icon = toggle.querySelector("i");
+//     toggles.forEach(function (toggle) {
+//         toggle.addEventListener("click", function () {
+//             const group = toggle.closest(".group");
+//             const dd = group.querySelector("dd");
+//             const icon = toggle.querySelector("i");
 
-            // Toggle class hidden untuk menampilkan/menyembunyikan dd
-            dd.classList.toggle("hidden");
+//             // Toggle class hidden untuk menampilkan/menyembunyikan dd
+//             dd.classList.toggle("hidden");
 
-            // Toggle rotasi ikon
-            icon.classList.toggle("-rotate-180");
-            icon.classList.toggle("rotate-0"); // pastikan class ini disiapkan di CSS
-        });
-    });
-});
+//             // Toggle rotasi ikon
+//             icon.classList.toggle("-rotate-180");
+//             icon.classList.toggle("rotate-0"); // pastikan class ini disiapkan di CSS
+//         });
+//     });
+// });
 
 Alpine.plugin(collapse);
 
